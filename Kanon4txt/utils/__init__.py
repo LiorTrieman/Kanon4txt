a4eaabdb8b0d0cf381fe34d23aab9b6b28388e83
Lior T <liortr30@gmail.com>
2023-06-27 13:37:53 +0300

    update init

--- Kanon4txt/utils/__init__.py
+++ Kanon4txt/utils/__init__.py
@@ -3,9 +3,9 @@
 # import Kanon4txt.utils as utils
 # empty init file
 # Path: __init__.py
-from anonym_utils import *
-from cluster_utils import *
-from llm_utils import *
-from models import *
-from nlp_utils import *
-from utilization_utils import *
+from Kanon4txt.utils.anonym_utils import *
+from Kanon4txt.utils.cluster_utils import *
+from Kanon4txt.utils.llm_utils import *
+from Kanon4txt.utils.models import *
+from Kanon4txt.utils.nlp_utils import *
+from Kanon4txt.utils.utilization_utils import *
